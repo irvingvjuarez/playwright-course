a4448c6233917b620982e02d50770a7c63f0e55f
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-14 23:16:33 -0600

    Assert test created

---
+++ tests/assert.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test"
+
+const BASE_URL = "http://uitestingplayground.com/"
+
+test.beforeEach(async ({ page }) => {
+	await page.goto(BASE_URL + "textinput")
+})
+
+test.describe("Using assertions", () => {
+	test("Expect", async ({ page }) => {
+		const contentText = "Juárez"
+		await page.type("input.form-control", contentText)
+
+		const changingBtn = page.locator("#updatingButton")
+		await changingBtn.click()
+		await expect(changingBtn).toHaveText(contentText)
+	})
+})
